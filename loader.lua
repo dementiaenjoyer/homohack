138ad8d24619f504dfde7b8da04049b1b0d1303f
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-06-26 12:46:26 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,6 +5,9 @@ local GameId = game.GameId
 
 --// Tables
 
+local Cfg = {
+    Loaded = false,
+}
 local Games = {
     { name = "Bad Business", gameid = 1168263273, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/bad%20business.lua" },
     { name = "Phantom Forces", gameid = 113491250, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/homohack.lua" },
@@ -21,5 +24,10 @@ for i, Supported in Games do
     if Supported.gameid == GameId then
         Library:Notify("Homohack detected you being in " .. Supported.name .. ", now loading script...", 5)
         loadstring(Fetch(Supported.link))()
+        Cfg["Loaded"] = true
     end
+end
+if not Cfg["Loaded"] then
+    Library:Notify("You are not in a homohack supported game, loading universal...")
+    loadstring(Fetch("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/universal.lua"))()
 end
