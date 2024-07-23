a30a42e57ee614bdcaa6861baeef93dbebceafd4
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-07-23 18:52:09 +0000

    Update rewrite.lua

--- rewrite.lua
+++ rewrite.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/homohack.lua")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/homohack.lua"))()
