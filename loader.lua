66f54f2fe19336a568d842641fa5e2ada105452f
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-08-08 17:14:08 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,9 +7,8 @@ local GameId = game.GameId
 
 local Games = {
     { name = "Rivals", gameid = 6035872082, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/rivals.lua" },
-    { name = "Bad Business", gameid = 1168263273, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/bad%20business.lua" },
-    { name = "Phantom Forces", gameid = 113491250, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/placeholder/chams.lua", rewrite = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/placeholder/chams.lua" },
-    { name = "Phantom Forces Test Place", gameid = 115272207, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/placeholder/chams.lua" },
+    { name = "Phantom Forces", gameid = 113491250, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/homohack.lua"},
+    { name = "Phantom Forces Test Place", gameid = 115272207, link = "https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/homohack.lua" },
 }
 
 function Fetch(URL)
@@ -20,17 +19,12 @@ end
 
 for _, Supported in Games do
     if Supported.gameid == GameId then
-        Library:Notify("Homohack detected you being in " .. Supported.name .. ", now loading script...", 5)
-
-        if Supported.name:find("Phantom Forces") and getgenv()["load_rewrite"] then
-            loadstring(Fetch(Supported.rewrite))()
-            return "loaded rewrite"
-        end
+        Library:Notify(`homohack has detected you being in {Supported.name}.`, 5)
 
         loadstring(Fetch(Supported.link))()
         return "loaded regular"
     end
 end
 
-Library:Notify("You are not in a homohack supported game, loading universal...")
+Library:Notify("You are not in a homohack supported game, loading universal..")
 loadstring(Fetch("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/universal.lua"))()
