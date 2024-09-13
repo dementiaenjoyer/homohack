f04a6de01b9ca64053f437173758e4948bd200f1
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2024-09-13 19:35:30 +0000

    Update homohack.lua

--- homohack.lua
+++ homohack.lua
@@ -1,1 +1,1 @@
-loadstring(Fetch("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/loader.lua"))()
