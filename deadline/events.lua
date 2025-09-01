1b332bb30dff8abb224bfba3f1c2b3f2b252f60b
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-09-01 08:18:32 +0200

    Update events.lua

--- deadline/events.lua
+++ deadline/events.lua
@@ -6,6 +6,7 @@ local Events = {
   ["PlayerAdded"] = "818838.0933546739",
   ["OnHit"] = "650209.529190766",
   ["Shoot"] = "549464.2024419813",
+  ["Equip"] = "805701.191854784",
 };
 
 return Namespaces, Events;
