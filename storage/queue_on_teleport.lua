3bc91c46a77a346e5adb86c77187b2184380b15b
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-10-16 13:04:47 +0200

    Update queue_on_teleport.lua

--- storage/queue_on_teleport.lua
+++ storage/queue_on_teleport.lua
@@ -5,14 +5,12 @@ while (not Players.LocalPlayer) do
     task.wait();
 end;
 
-writefile("homohack_script.lua", string.match(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/refs/heads/main/pf_main.lua"), "%[%=%=%=%[(.-)%]%=%=%=%]"));
-
 (QueueOnTeleport or function() end)([==[
     if (game.GameId ~= 113491250) then
         return;
     end;
     
-    loadstring(readfile("homohack_script.lua"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/refs/heads/main/storage/queue_on_teleport.lua"))();
 ]==]);
 
-return loadstring(readfile("homohack_script.lua"))();
+return loadstring(string.match(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/refs/heads/main/pf_main.lua"), "%[%=%=%=%[(.-)%]%=%=%=%]"))();
