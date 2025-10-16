a6bfd0f9ae5cbb94fb58438caf32b92d16c5d4be
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-10-16 13:09:53 +0200

    Update queue_on_teleport.lua

--- storage/queue_on_teleport.lua
+++ storage/queue_on_teleport.lua
@@ -6,8 +6,14 @@ while (not Players.LocalPlayer) do
 end;
 
 (QueueOnTeleport or function() end)([==[
+    local Players = cloneref(game:GetService("Players"));
+
     if (game.GameId ~= 113491250) then
         return;
+    end;
+
+    while (not Players.LocalPlayer) do
+        task.wait();
     end;
     
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/refs/heads/main/storage/queue_on_teleport.lua"))();
