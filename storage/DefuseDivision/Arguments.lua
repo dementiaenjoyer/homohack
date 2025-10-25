a287ac5cdc811f6ec924b044a579f23831910016
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-10-25 18:08:04 +0200

    Update Arguments.lua

--- storage/DefuseDivision/Arguments.lua
+++ storage/DefuseDivision/Arguments.lua
@@ -1,1 +1,1 @@
-return { "don\'t worry I\'ll rewrite it soon ;3", "CHARACTER", "DAMAGE", "GUNCLIENT", "HITPART", true, false, false, "REGCOUNT", "WEAPON" };
+return "don\'t worry I\'ll rewrite it soon ;3";
