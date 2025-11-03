e3efb68a731b7edce2d6bbd70148b2c4ccde0ced
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-11-03 16:46:37 +0100

    Update SaveManager.lua

--- storage/SaveManager.lua
+++ storage/SaveManager.lua
@@ -1,4 +1,4 @@
-local httpService = game:GetService('HttpService')
+local httpService = cloneref(game:GetService('HttpService'));
 
 local SaveManager = {} do
 	SaveManager.Folder = 'LinoriaLibSettings'
