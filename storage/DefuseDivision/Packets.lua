bcf3e353c55d36f03516490e12cad0a43f80d34c
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-11-16 13:28:27 +0100

    Update Packets.lua

--- storage/DefuseDivision/Packets.lua
+++ storage/DefuseDivision/Packets.lua
@@ -1,6 +1,6 @@
 return {["Hit"] = {
     "Weapon",
-    "Workspace:GetServerTimeNow()",
+    "ServerTime",
     { "Origin" },
     {
         {
@@ -16,6 +16,6 @@ return {["Hit"] = {
         false, "RegistrationCount", { 1 }, false -- i am pretty sure the '1' represents the pelletcount, but i am not entirely sure.
     },
     {
-        {Y = "Destination.Y", X = "Destination.X", Z = "Destination.Z"}
+        {Y = "DestinationY", X = "DestinationX", Z = "DestinationZ"}
     }
 }};
