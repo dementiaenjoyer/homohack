1f1b5f86d8bd914860205568878189de0d07c8f4
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2025-11-16 13:52:07 +0100

    Update Packets.lua

--- storage/DefuseDivision/Packets.lua
+++ storage/DefuseDivision/Packets.lua
@@ -16,6 +16,6 @@ return {["Hit"] = {
         false, "RegistrationCount", { 1 }, false -- i am pretty sure the '1' represents the pelletcount, but i am not entirely sure.
     },
     {
-        {Y = "OriginY", X = "OriginX", Z = "OriginZ"}
+        {Y = "DestinationY", X = "DestinationX", Z = "DestinationZ"}
     }
 }};
