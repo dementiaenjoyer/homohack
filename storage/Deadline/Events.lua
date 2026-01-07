07ed73d26c341a82c86ca8bf665ae27956d240e5
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2026-01-07 14:44:36 +0100

    Update Events.lua

--- storage/Deadline/Events.lua
+++ storage/Deadline/Events.lua
@@ -3,7 +3,7 @@ local Namespaces = {
 };
 
 local Events = {
-    ["Players"] = { ["Getter"] = "932438.1553287813", ["InflictHit"] = "650209.529190766", ["ShotFired"] = "313420.2700208304" }
+    ["Players"] = { ["Getter"] = "932438.1553287813", ["InflictHit"] = "133758.26977966932", ["ShotFired"] = "468681.9235070626", ["CycleRound"] = "251884.5282889347", ["EjectShell"] = "775912.8979508103", ["SingleShotReload"] = "421323.67325234675" }
 };
 
 return Namespaces, Events;
