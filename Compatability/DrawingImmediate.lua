480a2e34c9cd403ffa372c42829228b120056d41
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2026-06-08 01:29:20 +0200

    Removed debug things

--- Compatability/DrawingImmediate.lua
+++ Compatability/DrawingImmediate.lua
@@ -21,10 +21,7 @@ local Result = { }; do
 						continue;
 					end
 
-					print( ClassName );
-
 					local DrawingObject = BetterDrawing : Create( ClassName );
-					
 					DrawingObject.Visible = true;
 
 					for Property, Value in RenderData do
